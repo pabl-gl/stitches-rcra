8c77200749840a69e6a8ca21936a886b3b669b9f
Pablo García López <pablo.glopez@udc.es>
2026-03-07 20:34:45 +0100

    edit solve.sh

--- solve.sh
+++ solve.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-# Pablo García López pablo.glopez@udc.es
-# Ansur Lopez Braña mail
-
 ENCODINGS_DIR="encodings"
 SOLUTIONS_DIR="solutions"
 EXAMPLES_DIR="examples"
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+RESET='\033[0m'
+
+CHECK="[✔]"
+CROSS="[✖]"
 
 mkdir -p "$SOLUTIONS_DIR"
 
@@ -12,27 +16,33 @@ if [[ -z "$1" ]]; then
     for file in "$ENCODINGS_DIR"/dom*.lp; do
         num=$(basename "$file" .lp | sed 's/dom//')
         output="$SOLUTIONS_DIR/sol${num}.txt"
-        echo "Resolviendo: $file"
+
+        echo "Solving: $file"
         python3 decode.py stitches.lp "$file" "$output"
+
         if [ -f "$EXAMPLES_DIR/sol${num}.txt" ]; then
             if diff "$output" "$EXAMPLES_DIR/sol${num}.txt" > /dev/null; then
-                echo "dom${num} CORRECTA"
+                echo -e "${GREEN}${CHECK} dom${num} solution is correct${RESET}"
             else
-                echo "dom${num} DIFIERE de la esperada"
+                echo -e "${RED}${CROSS} dom${num}  solution differs from expected output${RESET}"
             fi
+            echo "---------------------------"
         fi
     done
 else
     num=$(printf "%02d" "$1")
     file="$ENCODINGS_DIR/dom${num}.lp"
     output="$SOLUTIONS_DIR/sol${num}.txt"
-    echo "Resolviendo: $file"
+
+    echo "Solving: $file"
     python3 decode.py stitches.lp "$file" "$output"
+
     if [ -f "$EXAMPLES_DIR/sol${num}.txt" ]; then
         if diff "$output" "$EXAMPLES_DIR/sol${num}.txt" > /dev/null; then
-            echo "dom${num} CORRECTA"
+            echo -e "${GREEN}${CHECK} dom${num} solution is correct${RESET}"
         else
-            echo "dom${num} DIFIERE de la esperada"
+            echo -e "${RED}${CROSS} dom${num}  solution differs from expected output${RESET}"
         fi
+        echo "---------------------------"
     fi
 fi
